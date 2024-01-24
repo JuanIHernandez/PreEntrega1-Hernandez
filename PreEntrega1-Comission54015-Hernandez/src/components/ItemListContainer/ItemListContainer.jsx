0dcc612629a4cf44155abbdaba87fb042960c1de
Juan Ignacio Hernandez <juan.i.hernandez@live.com.ar>
2024-01-24 15:23:46 -0300

    add components and bootstrap

---
+++ PreEntrega1-Comission54015-Hernandez/src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ItemListContainer = (props) => {
+  return (
+    <div>
+      <h2>{props.greetingMessage}</h2>
+    </div>
+  );
+};
+
+export default ItemListContainer;
