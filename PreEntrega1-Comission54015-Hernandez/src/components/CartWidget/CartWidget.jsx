c2b56cecb82ef675f1bcaf460ae8e36470945dfb
Juan Ignacio Hernandez <juan.i.hernandez@live.com.ar>
2024-01-24 18:25:46 -0300

    last commit

--- PreEntrega1-Comission54015-Hernandez/src/components/CartWidget/CartWidget.jsx
+++ PreEntrega1-Comission54015-Hernandez/src/components/CartWidget/CartWidget.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BsCart } from "react-icons/bs";
-
+import { useState } from "react";
+import style from "../CartWidget/CartWidget.module.css";
 const CartWidget = () => {
+  const [count, setCount] = useState(2);
   return (
     <div className="container">
-      <button>
-        <BsCart />
+      <button className={style.cartRight}>
+        <BsCart className={style.cartRight} />
+        <strong>{count}</strong>
       </button>
     </div>
   );
--- PreEntrega1-Comission54015-Hernandez/src/components/navbar/NavBar.jsx
+++ PreEntrega1-Comission54015-Hernandez/src/components/navbar/NavBar.jsx
@@ -15,9 +15,9 @@ const NavBar = () => {
           </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link href="#pricing">
+            <Nav.Link href="#Martillos">Features</Nav.Link>
+            <Nav.Link href="#LLaves">Pricing</Nav.Link>
+            <Nav.Link href="#Cart">
               <CartWidget />
             </Nav.Link>
           </Nav>
